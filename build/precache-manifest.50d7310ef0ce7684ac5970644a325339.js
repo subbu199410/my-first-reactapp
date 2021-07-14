f50aff99559894e910396e610d27a1ed6a2bc720
subbu199410 <74229167+subbu199410@users.noreply.github.com>
2021-07-14 13:22:33 +0530

    Add files via upload

---
+++ build/precache-manifest.50d7310ef0ce7684ac5970644a325339.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "397a023b7f7982078edfefa40ad8db5d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f633d4de0ab01fba9043",
+    "url": "/static/css/main.e6d6e83d.chunk.css"
+  },
+  {
+    "revision": "e593cceb243be3665793",
+    "url": "/static/js/2.24592da8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.24592da8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f633d4de0ab01fba9043",
+    "url": "/static/js/main.aeaa29b2.chunk.js"
+  },
+  {
+    "revision": "ebdcd77d421a986d2d21",
+    "url": "/static/js/runtime-main.c8b63f78.js"
+  }
+]);
